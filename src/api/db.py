2c7c0f0d1147685986cc59c7a0ba9a961810ce28
Monique Menezes <122538719+Niquemenezes@users.noreply.github.com>
2025-10-08 09:07:27 +0000

    app.py

--- src/api/db.py
+++ src/api/db.py
@@ -1,2 +1,5 @@
-from flask_sqlalchemy import SQLAlchemy
-db = SQLAlchemy()
+
+import os
+from sqlalchemy import create_engine
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///dev.db")
+engine = create_engine(DATABASE_URL, echo=False, future=True)
